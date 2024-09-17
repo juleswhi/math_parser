79c469c98ee40779fcd75fa8982e00eba40a3425
jules white <jules.white1@protonmail.com>
2024-09-17 20:32:34 +0100

    Create math file and read it in

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+    std.debug.print("Input: \n", .{});
+    const al = std.heap.page_allocator;
+    const data = try read_file("math", &al);
+
+    std.debug.print("{s}\n", .{data});
+}
+
+fn read_file(filename: []const u8, alloc: *const std.mem.Allocator) ![]const u8 {
+    var data = std.ArrayList(u8).init(alloc.*);
+    defer data.deinit();
+    const file = std.fs.cwd().openFile(filename, .{}) catch |err| {
+        std.log.err("Failed to open file: {s}", .{@errorName(err)});
+        return err;
+    };
+    defer file.close();
+
+    while (file.reader().readUntilDelimiterOrEofAlloc(alloc.*, '\n', std.math.maxInt(usize)) catch |err| {
+        std.log.err("Failed to read line: {s}", .{@errorName(err)});
+        return err;
+    }) |line| {
+        defer alloc.*.free(line);
+        try data.appendSlice(line);
+    }
+
+    return data.toOwnedSlice();
 }
